1c765d60e592e944f18fe6a7614bcab605520e04
Hieu Tran <hieutran6102@gmail.com>
2024-02-27 01:13:03 +0700

    add models

---
+++ backend/models/order.js
@@ -0,0 +1,76 @@
+import mongoose from 'mongoose';
+
+const orderSchema = mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User',
+        },
+        orderItems: [
+            {
+                product: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    required: true,
+                    ref: 'Product',
+                quantity: { type: Number, required: true },
+                itemPrice: { type: Number, required: true },
+                },
+            },
+        ],
+        shippingAddress: {
+            detailAddress: { type: String, required: true },
+            city: { type: String, required: true },
+            district: { type: String, required: true },
+            ward: { type: String, required: true },
+        },
+        paymentMethod: {
+            type: String,
+            required: true,
+        },
+        paymentResult: {
+            id: { type: String },
+            status: { type: String },
+            update_time: { type: String },
+            email_address: { type: String },
+        },
+        taxPrice: {
+            type: Number,
+            required: true,
+            default: 0.0,
+        },
+        shippingPrice: {
+            type: Number,
+            required: true,
+            default: 0.0,
+        },
+        totalPrice: {
+            type: Number,
+            required: true,
+            default: 0.0,
+        },
+        isPaid: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+        paidAt: {
+            type: Date,
+        },
+        isDelivered: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+        deliveredAt: {
+            type: Date,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
