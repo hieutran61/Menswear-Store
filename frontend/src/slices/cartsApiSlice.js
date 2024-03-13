ea3c56933da2f7e976214a0ff1edfdd10ce2227c
Pham Phuoc Tri <tribestst123@gmail.com>
2024-03-13 15:58:41 +0700

    fix: show image , product name, price in cart
    remove item cart not done

--- frontend/src/slices/cartsApiSlice.js
+++ frontend/src/slices/cartsApiSlice.js
@@ -2,27 +2,32 @@ import { apiSlice } from './apiSlice';
 import { CARTS_URL, PAYPAL_URL } from '../constants';
 
 export const cartApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        addItemToCart: builder.mutation({
-            query: (item) => ({
-                url: CARTS_URL,
-                method: 'POST',
-                body: item,
-            }),
-        }),
-        getCarts: builder.query({
-            query: () => ({
-                url: CARTS_URL,
-                method: 'GET',
-            }),
-            keepUnusedDataFor: 1,
-        }),
-
+  endpoints: (builder) => ({
+    addItemToCart: builder.mutation({
+      query: (item) => ({
+        url: CARTS_URL,
+        method: 'POST',
+        body: item,
+      }),
     }),
+    getCarts: builder.query({
+      query: () => ({
+        url: CARTS_URL,
+        method: 'GET',
+      }),
+      keepUnusedDataFor: 1,
+    }),
+    removeCartItem: builder.mutation({
+      query: (itemId) => ({
+        url: `${CARTS_URL}/${itemId}`,
+        method: 'DELETE',
+      }),
+    }),
+  }),
 });
 
 export const {
-    useAddItemToCartMutation,
-    useGetCartsQuery,
-
+  useAddItemToCartMutation,
+  useGetCartsQuery,
+  useRemoveCartItemMutation,
 } = cartApiSlice;
