1cb12598d481d1682a8a24701ef7fc5b233a8766
novami15 <ngovanminh15062002@gmail.com>
2024-03-18 02:04:50 +0700

    Edit Create product  and Update product API

--- backend/models/product.js
+++ backend/models/product.js
@@ -30,6 +30,10 @@ const productSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    brand: {
+      type: String,
+      required: true,
+    },
     reviews: [reviewSchema],
     rating: {
       type: Number,
